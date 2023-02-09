ff29ba939dbd02154bb75d41a103b3b360d0fb6a
Muthengiajoy <prof.joy7079@gmail.com>
2023-02-09 11:20:52 +0300

    lesso 4

---
+++ condition.py
@@ -0,0 +1,9 @@
+num = eval(input("Enter a number:66"))
+if num < 0:
+    print("wrong input")
+else:
+    if num > 50:
+        x = num/2
+        print(x)
+    else:
+        print("print number is less than 50")
---
+++ userinput.py
@@ -0,0 +1,4 @@
+temp=eval(input("20:"))
+print(."the temperature is:",temp)
+age=input("18:")
+print(."my miaka is:",age)
