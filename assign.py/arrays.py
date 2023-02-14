75137106d927a2964d0b700004607aba1db00a44
Muthengiajoy <prof.joy7079@gmail.com>
2023-02-14 09:25:33 +0300

    git commit

--- assign.py/arrays.py
+++ assign.py/arrays.py
@@ -4,11 +4,15 @@
 #numbers = [3,5,6,9]
 #numbers[2] = 8
 #print(numbers)
-#cars = ["vanguard","isuzu"]
+cars = ["vanguard","isuzu"]
 #cars.append("fortuner")
 #print(cars)
-nums = [2,3,4,5,7]
-nums.pop(0)
-print(nums)
-nums.remove(3)
-print(nums)
+#nums = [2,3,4,5,7]
+#nums.pop(0)
+#print(nums)
+#nums.remove(3)
+#print(nums)
+nums = [3,4,5,6,7,8,9,911]
+length = (len(nums))
+print(length)
+print(len(cars))
