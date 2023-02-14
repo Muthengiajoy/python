bcd358af6fa79e7173ea81cabe477e1d923fdecb
Muthengiajoy <prof.joy7079@gmail.com>
2023-02-14 10:50:41 +0300

    git commit

---
+++ assign.py/strings.py/string1.py
@@ -0,0 +1,39 @@
+#name = "climate"
+#print(name[0])
+#print(len(name))
+#print(name[10])
+#print(name[-1])
+#for letter in name:
+    #print(letter)
+#print("_"*9)
+#name1 = "joy"
+#name2 = "mary"
+#print(name1 + name2)
+#nam = input("enter name:")
+#print("hello" + nam)
+#name3 = "liza"
+#name4 = "tim"
+#print(name3)
+#print("/n")
+#print(name4)
+#string methods-upper,lower,isalpha
+#name4 = "JOY"
+#print(name4.lower())
+#checking for alphanumerics
+#name = "number1tv"
+#print(name.isalpha())
+#password = "string345"
+#if password.isalpha():
+    #print("invalid password")
+#else:
+    #print("valid password")
+#def my_function(user_name):
+    #user_name = input("enter_name")
+    #print("HELLO" + user_name)
+#my_function("user_name")
+email = "bob123@gmail.com"  
+print(email[7:12])
+print(email[::-1]) 
+ 
+
+
