d3e2861848fc0ba49535d96bbcb39bd425d29376
Muthengiajoy <prof.joy7079@gmail.com>
2023-02-20 11:53:40 +0300

    assignment on dictionaries

--- assign.py/cars/dictionary.py
+++ assign.py/cars/dictionary.py
@@ -8,7 +8,7 @@
 #person = {"name":"Joy","age":"18","pets":{"name":"tom","name1":"scar"}}
 #print(person["pets"]["name1"])
 #teams = {23:"as"}
-profile = {}
+#profile = {}
 #profile["username"] = "Muthengia"
 #profile["age"] = "18"
 #profile["email"] = "prof.joy7079@gmail.com"
@@ -17,24 +17,47 @@ profile = {}
 
     #ask for user name,email,password n store in a dictionary
 
-def sign_up():
-    username = input("enter username:")
-    email = input("enter_email:")
-    password = input("enter_password:")
+#def sign_up():
+    #username = input("enter username:")
+    #email = input("enter_email:")
+    #password = input("enter_password:")
     
-    profile["username"] = username
-    profile["email"] = email
-    profile["password"] = password
-    print(profile)
+    #profile["username"] = username
+    #profile["password"] = password
+    #print(profile)
     
-sign_up()
+#sign_up()
 
-def change_username():
-    new_username = input("enter new_username:")
-    profile["username"] = new_username
-    print(profile)
+#def change_username():
+   # new_username = input("enter new_username:")
+    #profile["username"] = new_username
+    #print(profile)
 
-change_username()
+#change_username()
+#def product_s():
+    #products = input("enter products:")
+    #prices = input("enter prices:")
+
+    #profile["products"] = products
+    #profile["prices"] = prices
+    #print(profile)
+
+#product_s()
+
+year = {"january":"31","february":"28","march":"31"}
+#month_name = input("month")
+#if month_name == "january":
+    #print(31)
+#elif month_name == "february":
+#    print(28)
+mykeys = list(year.keys())
+mykeys.sort()
+for i in mykeys:
+    print(mykeys)
+
+
+    
+
 
 
 
